aa47e0f97e203284fa5a5ecbaa2254b9d90056de
Florian Panzer <florian.panzer@bsi-software.com>
2021-02-12 17:08:53 +0100

    Add Merchent UI

---
+++ Assets/Scripts/Controller/MerchantController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MerchantController : MonoBehaviour
+{
+    public GameObject prefabMerchantSlot;
+
+    private Transform MerchantPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.tag = "Merchant";
+        MerchantPanel = GameObject.Find("MerchantPanel").transform;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
